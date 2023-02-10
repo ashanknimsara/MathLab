c6d22143bf25759536268472f151acc92df08a7b
Ashan K Nimsara <ashanknimsara@gmail.com>
2023-02-10 23:09:44 +0530

    backend

---
+++ backend/src/api/routes/index.js
@@ -0,0 +1,13 @@
+const routesInit = (app, passport)=>{
+    app.get("/auth/google" ,passport.authenticate("google",{scope:["profile","email"]}));
+    app.get(
+        "/auth/google/callback", passport.authenticate("google",{
+        failureRedirect:"/login",
+    }),
+    (req, res)=>{
+        console.log("User Authenticated");
+    }
+    );
+};  
+
+export {routesInit}
